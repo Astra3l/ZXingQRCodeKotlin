34da9ad196e5651029cef0390e03edca9d64fa70
riyas <riyas.vmc@gmail.com>
2021-10-18 22:20:54 +0530

    updated dependencies.

--- app/src/main/java/com/vmcplus/zxingkotlin/MainActivity.kt
+++ app/src/main/java/com/vmcplus/zxingkotlin/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val codeWriter = MultiFormatWriter()
         try {
-            val bitMatrix = codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height)
+            val bitMatrix = codeWriter.encode(text, BarcodeFormat.CODE_128, width, height)
             for (x in 0 until width) {
                 for (y in 0 until height) {
                     bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
